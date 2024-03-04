47df851c305abdc1e3722a9a3dcfb7231518660a
yuriko810 <yuriko@majorana.systems>
2024-03-03 19:33:20 -0500

    Build a minimal freestanding binary.

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+#![no_std]
+#![no_main]
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+use core::panic::PanicInfo;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
